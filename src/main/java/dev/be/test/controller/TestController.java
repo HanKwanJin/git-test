09f4065503d4ee268e14ecde2f91c8b1fda8be59
hankwanjin <49313910+HanKwanJin@users.noreply.github.com>
2023-07-05 21:45:22 +0900

    Feature/#3/two (#4)
    * feature: [#3] 두번째 기능 개발 완료
    
    * feature: [#3] 세번째 기능 개발 완료

--- src/main/java/dev/be/test/controller/TestController.java
+++ src/main/java/dev/be/test/controller/TestController.java
@@ -10,4 +10,14 @@ public class TestController {
     public String featureOne() {
         return "git-test";
     }
+
+    @GetMapping("/two")
+    public String featureTwo() {
+        return "git-test-test";
+    }
+
+    @GetMapping("/three")
+    public String featureThree() {
+        return "three";
+    }
 }
